0733764fa5a04630431b4997be57d3eade88224d
allexander-w <work.1793593@gmail.com>
2023-01-13 08:46:59 +0600

    minified js, css files

--- js/main.js
+++ js/main.js
@@ -10,14 +10,6 @@ const swiper = new Swiper(".swiper", {
     prevEl: '.swiper-button-prev',
   },
   breakpoints: {
-    // 320: {
-    //   slidesPerView: 2,
-    //   spaceBetween: 20
-    // },
-    // 480: {
-    //   slidesPerView: 3,
-    //   spaceBetween: 30
-    // },
     320: {
       slidesPerView: 2,
       grid: {
